46201ec3da7d94e06e4a091354471353958274d5
bregarulai <bregarulai@gmail.com>
2024-07-18 01:00:52 -0400

    implement main and root layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <h1>Evently</h1>
-    </main>
+    </>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={poppins.variable}>{children}</body>
