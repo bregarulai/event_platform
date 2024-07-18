35fe9e75dc3dfbee88d64bce3de1c2f9e33dbb17
bregarulai <bregarulai@gmail.com>
2024-07-18 00:06:14 -0400

    updated tailwing.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,13 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
   prefix: "",
   theme: {
     container: {
@@ -25,9 +25,20 @@ const config = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "hsl(var(--primary))",
+          500: "#624CF5",
+          50: "#F6F8FD",
           foreground: "hsl(var(--primary-foreground))",
         },
+        coral: {
+          500: "#15BF59",
+        },
+        grey: {
+          600: "#545454",
+          500: "#757575",
+          50: "#F6F6F6",
+        },
+        black: "#000000",
+        white: "#FFFFFF",
         secondary: {
           DEFAULT: "hsl(var(--secondary))",
           foreground: "hsl(var(--secondary-foreground))",
@@ -53,6 +64,13 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      fortFamily: {
+        poppins: ["var(--font-poppins)"],
+      },
+      backgroundImage: {
+        "dotted-pattern": "url('assets/images/dotted-pattern.png')",
+        "hero-img": "url('/assets/images/hero.png')",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
@@ -75,6 +93,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
