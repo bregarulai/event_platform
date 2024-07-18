8db018ce6bac856aacf52eaeb4d310897a7b648d
bregarulai <bregarulai@gmail.com>
2024-07-18 00:13:04 -0400

    updated tailwind.confi.ts with uploadthings

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,6 @@
-import type { Config } from "tailwindcss";
+import { withUt } from "uploadthing/tw";
 
-const config = {
+module.exports = withUt({
   darkMode: ["class"],
   content: [
     "./pages/**/*.{ts,tsx}",
@@ -93,6 +93,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config;
-
-export default config;
+});
