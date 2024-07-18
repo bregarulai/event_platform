28d85bbfa8e38a863013f392073366b796488f1c
bregarulai <bregarulai@gmail.com>
2024-07-18 02:29:19 -0400

    implement clerk initial configuration

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,8 +1,21 @@
+import Image from "next/image";
+import Link from "next/link";
+
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header className="w-full border-b">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/images/logo.svg"
+            height={38}
+            width={123}
+            alt="Evently logo"
+          />
+        </Link>
+        <div className="flex w-32 justify-end gap-3"></div>
+      </div>
+    </header>
   );
 };
 
