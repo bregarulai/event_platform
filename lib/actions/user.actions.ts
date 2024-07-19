89c11eba002bffe30af99ae8539c0712effc142d
bregarulai <bregarulai@gmail.com>
2024-07-18 23:28:47 -0400

    implement deleteUser action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -4,6 +4,9 @@ import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError, parseStringify } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
+import Event from "../database/models/event.model";
+import Order from "../database/models/order.model";
+import { revalidatePath } from "next/cache";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -41,6 +44,46 @@ export const updateUser = async (clerkId: string, user: UpdateUserParams) => {
     if (!updatedUser) throw new Error("User update failed");
 
     return parseStringify(updatedUser);
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const deleteUser = async (clerkId: string) => {
+  try {
+    await connectToDatabase();
+
+    // Find user to delete
+    const userToDelete = await User.findOne({ clerkId });
+
+    if (!userToDelete) throw new Error("User not found");
+
+    // Unlink relationships
+    await Promise.all([
+      // Update the 'event' collection to remove references to the user
+      Event.updateMany(
+        { _id: { $in: userToDelete.events } },
+        { $pull: { organizer: userToDelete._id } }
+      ),
+
+      // Update the 'orders' collection to remove references to the user
+      Order.updateMany(
+        {
+          _id: {
+            $in: userToDelete.orders,
+          },
+        },
+        {
+          $unset: { buyer: 1 },
+        }
+      ),
+    ]);
+
+    // Delete user
+    const deletedUser = await User.findByIdAndDelete(userToDelete._id);
+
+    revalidatePath("/");
+    return deletedUser ? parseStringify(deletedUser) : null;
   } catch (error) {
     handleError(error);
   }
