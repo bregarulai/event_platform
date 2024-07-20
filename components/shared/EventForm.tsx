ea39c2faf7ded193ba5580fe818ec171dd34d3a2
bregarulai <bregarulai@gmail.com>
2024-07-19 20:10:22 -0400

    implement submit button styles

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -275,7 +275,14 @@ const EventForm = ({ userId, type }: EventFormProps) => {
             )}
           />
         </div>
-        <Button type="submit">Submit</Button>
+        <Button
+          className="button col-span-2 w-full"
+          type="submit"
+          size="lg"
+          disabled={form.formState.isSubmitting}
+        >
+          {form.formState.isSubmitting ? "Submitting" : `${type} Event`}
+        </Button>
       </form>
     </Form>
   );
