37a6761e03144b8bef1af57699d3381fb8d1d187
bregarulai <bregarulai@gmail.com>
2024-07-19 20:03:05 -0400

    implement isFree checkbox, url field

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -23,6 +23,7 @@ import { eventDefaulValues } from "@/constants";
 import Dropdown from "./Dropdown";
 import { Textarea } from "../ui/textarea";
 import FileUploader from "./FileUploader";
+import { Checkbox } from "../ui/checkbox";
 
 const EventForm = ({ userId, type }: EventFormProps) => {
   const [files, setFiles] = useState<File[]>([]);
@@ -218,6 +219,53 @@ const EventForm = ({ userId, type }: EventFormProps) => {
                       className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0"
                       type="number"
                       placeholder="price"
+                      {...field}
+                    />
+                    <FormField
+                      control={form.control}
+                      name="isFree"
+                      render={({ field }) => (
+                        <FormItem>
+                          <FormControl>
+                            <div className="flex items-center">
+                              <label
+                                className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                htmlFor="isFree"
+                              >
+                                Free Ticket
+                              </label>
+                              <Checkbox
+                                id="isFree"
+                                className="mr-2 h-5 w-5 border-2 border-primary-500"
+                              />
+                            </div>
+                          </FormControl>
+                          <FormMessage />
+                        </FormItem>
+                      )}
+                    />
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="url"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-gray-50 px-4 py-2">
+                    <Image
+                      src="/assets/icons/link.svg"
+                      height={24}
+                      width={24}
+                      alt="link"
+                    />
+                    <Input
+                      className="input-field"
+                      placeholder="URL"
                       {...field}
                     />
                   </div>
