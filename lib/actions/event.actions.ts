346cfe5eaae6841dd90e0e8a585d489cec65b388
bregarulai <bregarulai@gmail.com>
2024-07-22 13:21:18 -0400

    implement getEventById action, EventDetails page

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,17 @@ import { handleError, parseStringify } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -25,6 +36,22 @@ export const createEvent = async ({
       organizer: userId,
     });
     return parseStringify(newEvent);
+  } catch (error) {
+    handleError(error);
+  }
+};
+
+export const getEventById = async (eventId: string) => {
+  try {
+    await connectToDatabase();
+
+    const event = await populateEvent(Event.findById(eventId));
+
+    if (!event) {
+      throw new Error("Event not found");
+    }
+
+    return parseStringify(event);
   } catch (error) {
     handleError(error);
   }
