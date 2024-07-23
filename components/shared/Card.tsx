ccb9eb3acd0f37d6fed30078ecea00fcab2211f2
bregarulai <bregarulai@gmail.com>
2024-07-22 20:07:16 -0400

    implement is creator section, DeleteConfirmation component

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,9 +1,17 @@
-import { formatDateTime } from "@/lib/utils";
-import { CardProps } from "@/types";
 import Image from "next/image";
 import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
+
+import { CardProps } from "@/types";
+import { formatDateTime } from "@/lib/utils";
+import DeleteConfirmation from "./DeleteConfirmation";
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const isEventCreator = userId === event.organizer._id.toString();
+
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
@@ -11,7 +19,19 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         href={`/events/${event._id}`}
         style={{ backgroundImage: `url(${event.imageUrl})` }}
       />
-      {/* TODO: IS EVENT CREATOR... */}
+      {isEventCreator && !hidePrice && (
+        <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+          <Link href={`/events/${event._id}/update`}>
+            <Image
+              src="/assets/icons/edit.svg"
+              height={20}
+              width={20}
+              alt="edit"
+            />
+          </Link>
+          <DeleteConfirmation eventId={event._id} />
+        </div>
+      )}
       <Link
         className="flex min-h-[230px] flex-col gap-3 p-5 md:gap-4"
         href={`/events/${event._id}`}
---
+++ components/shared/DeleteConfirmation.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+const DeleteConfirmation = ({ eventId }: { eventId: string }) => {
+  return (
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Image
+          src="/assets/icons/delete.svg"
+          height={20}
+          width={20}
+          alt="delete"
+        />
+      </AlertDialogTrigger>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Are you sure you want to delete?</AlertDialogTitle>
+          <AlertDialogDescription>
+            This will permanently delete this event
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>Delete</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
+
+export default DeleteConfirmation;
